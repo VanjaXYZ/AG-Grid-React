340cdca3862ae12dbee0e7e3aa34721dd925a4ef
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-07-20 01:10:07 +0200

    created initial layout

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,45 @@
+import { AgGridReact } from "ag-grid-react";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
 
 const Table = () => {
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "symbol" },
+    { field: "priceChange" },
+    { field: "priceChangePercent" },
+    { field: "weightedAvgPrice" },
+    { field: "prevClosePrice" },
+    { field: "lastPrice" },
+    { field: "lastQty" },
+    { field: "bidPrice" },
+    { field: "bidQty" },
+    { field: "askPrice" },
+    { field: "askQty" },
+    { field: "openPrice" },
+    { field: "highPrice" },
+    { field: "lowPrice" },
+    { field: "volume" },
+    { field: "quoteVolume" },
+    { field: "openTime" },
+    { field: "closeTime" },
+    { field: "firstId" },
+    { field: "lastId" },
+    { field: "count" },
+  ]);
+  const columnConfig = useMemo(
+    () => ({
+      sortable: true,
+    }),
+    []
+  );
+  const containerStyle = useMemo(
+    () => ({ width: "100%", height: "100vh" }),
+    []
+  );
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState([]);
   useEffect(() => {
     const getData = async () => {
       try {
@@ -9,6 +47,7 @@ const Table = () => {
           "https://data.binance.com/api/v3/ticker/24hr"
         );
         console.log(response.data);
+        setRowData(response.data);
       } catch (error) {
         console.error(error);
       }
@@ -17,8 +56,16 @@ const Table = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div style={containerStyle}>
+      <div className="ag-theme-alpine-dark" style={gridStyle}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={columnConfig}
+          animateRows={true}
+          pagination={true}
+        />
+      </div>
     </div>
   );
 };
